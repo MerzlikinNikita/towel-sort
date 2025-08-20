1ef83a819ddf982728a340e5ee34fd33b28d7039
Merzlikin Nikita <nikmerz2002@mail.ru>
2025-08-20 21:09:14 +0600

    feat: add a solution for the towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return [];
+  }
+
+  const resultArr = matrix.map((el, index) =>
+    index % 2 !== 0 ? el.reverse() : el
+  );
+
+  return resultArr.flat();
 };
